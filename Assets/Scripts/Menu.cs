45e649a5625176893fe7bcb9e58b81bf488c4bab
GustavoBNGodinho <gustavobngodinho@gmail.com>
2026-04-21 19:09:24 -0300

    Initial commit

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.Video;
+
+public class Menu : MonoBehaviour
+{
+    public VideoPlayer videoPlayer;
+    public GameObject menuOpcoes, rawImage;
+    void Start()
+    {
+        rawImage.SetActive(false);
+    }
+
+    
+    void Update()
+    {
+        if (!videoPlayer.isPlaying && Input.anyKeyDown)
+        {
+            Debug.Log("oi");
+            videoPlayer.Play();
+            rawImage.SetActive(true);
+            menuOpcoes.SetActive(true);
+        }
+    }
+}
